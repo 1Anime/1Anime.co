048a15eb32801ed38e35ce144f1d2f2eb8ef499f
dev1Abun <mavenphofficial@gmail.com>
2024-12-07 18:06:15 +0800

    Update RequestExamples.svelte

--- src/lib/components/RequestExamples.svelte
+++ src/lib/components/RequestExamples.svelte
@@ -89,6 +89,20 @@ function getFakeUrl(url) {
 }
 </script>
 
+<div class="request">
+    <Chip type="request" size="small">Speed Test</Chip>
+    <Typewriter
+            mode="loopOnce"
+            keepCursorOnFinish={true}
+            interval={20}
+            unwriteInterval={5}
+            on:done={apiCall}
+    ><code></code></Typewriter>
+    {#if showDuration && requestDuration > 0}
+        <span transition:fly={{delay:0,duration:150,x:-32}} class="duration">{requestDuration}ms</span>
+    {/if}
+</div>
+
 <div class="cards">
     {#if !showCards}
         {#each {length: 6} as _, i}
