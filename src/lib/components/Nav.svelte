6030b66c5b980b4c2f58a7fb256d2929788883e5
dev1Abun <mavenphofficial@gmail.com>
2024-12-07 18:13:43 +0800

    Update some UI

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -34,17 +34,19 @@ function toggleMobileNav() {
         <div class="nav__bodyWrapper">
             <div class="nav__body">
                 <div class="main">
-                    <Button
-                        type="nav-link"
-                        urlTarget="_blank"
-                        url="https://1anime.one"
-                    >
-                        <IconArrowDownLeft class="external-link" size={20} stroke={1.5} />
-                        Watch Now
-                        <Chip type="outline" size="small">v2.0</Chip>
-                    </Button>
                     <Button type="nav-link" url="/#features">Features</Button>
                     <Button type="nav-link" url="/#integrations">Integrations</Button>
+                    <Button
+                    type="nav-link"
+                    urlTarget="_blank"
+                    url="https://1anime.one"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+                    </svg>
+                    Watch Now
+                    <Chip type="outline" size="small">v2.0</Chip>
+                </Button>
                 </div>
 
                 <div class="links">
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -135,8 +135,10 @@
 			</BasicInfoDisplayGrouped>
 
 			<ButtonGroup>
-				<Button type="primary" urlTarget="_blank" url="https://docs.1anime.com/">
-					<IconArrowDownLeft class="external-link" size={20} stroke={1.5} />
+				<Button type="primary" urlTarget="_blank" url="https://1anime.one/">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+					  <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />
+					</svg>
 					Watch now
 				</Button>
 			</ButtonGroup>
