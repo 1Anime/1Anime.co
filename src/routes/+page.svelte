83be499ee7427dbd4ba80e024d10a8c63d26b34c
dev1Abun <mavenphofficial@gmail.com>
2024-12-18 17:27:00 +0800

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,15 +37,6 @@
 
 	console.log(github)
 
-	let titles = ["1Anime", "Ad-free", "Unlimited Anime", "Read Manga", "All for FREE"];
-	let currentIndex = 0;
-
-	function changeTitle() {
-		currentIndex = (currentIndex + 1) % titles.length;
-	}
-
-	setInterval(changeTitle, 5000); // Change title every 5 seconds
-
 </script>
 
 <svelte:head>
@@ -100,7 +91,16 @@
 			>Support us by Donating to keep 1Anime ad-free!</InlineBanner>
 
 			<div class="type--group-heading-and-content">
-				<h1 class="title-animation">{titles[currentIndex]}</h1>
+					<div class="loader">
+						<p>AniManga</p>
+						<div class="words">
+							<span class="word">without ads</span>
+							<span class="word">with large library</span>
+							<span class="word">reliable support</span>
+							<span class="word">next-level integrations</span>
+							<span class="word">ALL FOR FREE</span>
+						</div>
+				</div>
 				<p>Your ad-free platform for streaming the latest anime and manga, completely free of charge.</p>
 			</div>
 
@@ -157,7 +157,6 @@ Watch Now
 
 	<Marquee />
 </section>
-
 
 <section class="featuresWrapper" id="features">
 	<div class="features">
@@ -279,6 +278,93 @@ Watch Now
 <Footer />
 
 <style>
+	.card {
+		--bg-color: #212121;
+		background-color: var(--bg-color);
+		padding: 1rem 2rem;
+		border-radius: 1.25rem;
+	}
+	.loader {
+		color: rgb(124, 124, 124);
+		font-family: "Poppins", sans-serif;
+		font-weight: 500;
+		font-size: 25px;
+		-webkit-box-sizing: content-box;
+		box-sizing: content-box;
+		height: 40px;
+		padding: 10px 10px;
+		display: -webkit-box;
+		display: -ms-flexbox;
+		display: flex;
+		border-radius: 8px;
+	}
+
+	.words {
+		overflow: hidden;
+		position: relative;
+	}
+	.words::after {
+		content: "";
+		position: absolute;
+		inset: 0;
+		background: linear-gradient(
+			var(--bg-color) 10%,
+			transparent 30%,
+			transparent 70%,
+			var(--bg-color) 90%
+		);
+		z-index: 20;
+	}
+
+	.word {
+		display: block;
+		height: 100%;
+		padding-left: 6px;
+		color: #956afa;
+		animation: spin_4991 4s infinite;
+	}
+
+	@keyframes spin_4991 {
+		10% {
+			-webkit-transform: translateY(-102%);
+			transform: translateY(-102%);
+		}
+
+		25% {
+			-webkit-transform: translateY(-100%);
+			transform: translateY(-100%);
+		}
+
+		35% {
+			-webkit-transform: translateY(-202%);
+			transform: translateY(-202%);
+		}
+
+		50% {
+			-webkit-transform: translateY(-200%);
+			transform: translateY(-200%);
+		}
+
+		60% {
+			-webkit-transform: translateY(-302%);
+			transform: translateY(-302%);
+		}
+
+		75% {
+			-webkit-transform: translateY(-300%);
+			transform: translateY(-300%);
+		}
+
+		85% {
+			-webkit-transform: translateY(-402%);
+			transform: translateY(-402%);
+		}
+
+		100% {
+			-webkit-transform: translateY(-400%);
+			transform: translateY(-400%);
+		}
+	}
 	.title-animation {
 		background: linear-gradient(90deg, #0000ff, #800080);
 		-webkit-background-clip: text;
